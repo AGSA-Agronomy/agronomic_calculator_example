c666e0217f9f8c65936cde55b7dd2821e783061e
AGSA-Agronomy <agsa.agronomy@gmail.com>
2023-04-20 17:06:22 -0500

    work in progress plotting feature

--- calculator.py
+++ calculator.py
@@ -2,11 +2,10 @@ def acres_to_hectare(acres):
     """This function converts area in acres to hectares"""
     return acres * 0.404686
 
-def gwv_to_vwc(gwc, bd):
-    """ Convert Gravimetric Water Content into 
-    Volumetric Content using bulk density of soil"""
-    return gwc * bd
-def corn_bushel_acre_to_kg_hectare(bushels):
-    """ This function converts bushels of corn per acre to kg per hectar"""
-    kg_hectar = bushels *62.77
-    return kg_hectar
+def lb_to_kg(lb):
+    """FUnction to convert lb to kg"""
+    return lb * 0.453592
+
+def create_plot(yield_, years):
+    """FUnction to convert lb to kg"""
+    plt.plt(yield_, years)
