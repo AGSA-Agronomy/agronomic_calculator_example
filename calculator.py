0de31e2c37087814328fa17575f9b5ee75ac4a2f
Ignacio Massigoge <ignaciomassigoge@gmail.com>
2023-04-20 16:44:45 -0500

    Function to convert corn grain yield in bu/ac to kg/ha

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
 def acres_to_hectare(acres):
     """This function converts area in acres to hectares"""
     return acres * 0.404686
+
+def bu_ac_to_kg_ha(lb_ha):
+    """This function converts corn grain yield in bushel per acre to kg per hectare"""
+    return lb_ac / 0.0159
