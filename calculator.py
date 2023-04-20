fff412fd50a2001c7dff0eda23826a3f748f2fae
Gustavo-Roa <groa@ksu.edu>
2023-04-20 17:10:37 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,5 +8,5 @@ def gwv_to_vwc(gwc, bd):
     return gwc * bd
 
 def wheat_bu_to_wheat_ton(wbu, wton):
-    """ Convert Wheat bu to Wheat Ton"""
+    """ Convert Wheat (bu) to Wheat (Ton)"""
     return wbu * 0.027216
