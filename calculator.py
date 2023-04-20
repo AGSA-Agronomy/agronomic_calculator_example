0d161a7ab10b2a1526c52a9025d5a55a215366ea
giordanon <ngiordano@ksu.edu>
2023-04-20 16:39:21 -0500

    lb to kg function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
 def acres_to_hectare(acres):
     """This function converts area in acres to hectares"""
     return acres * 0.404686
+
+def lb_to_kg(lb):
+    """FUnction to convert lb to kg"""
+    return lb * 0.453592
