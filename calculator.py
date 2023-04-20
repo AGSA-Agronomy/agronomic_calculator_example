f0ccbc3e376483db02e280fb6108daf52a53e01c
AGSA-Agronomy <agsa.agronomy@gmail.com>
2023-04-20 16:35:49 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
 def acres_to_hectare(acres):
     """This function converts area in acres to hectares"""
     return acres * 0.404686
+
+def gwv_to_vwc(gwc, bd):
+    """ Convert Gravimetric Water Content into 
+    Volumetric Content using bulk density of soil"""
+    return gwc * bd
